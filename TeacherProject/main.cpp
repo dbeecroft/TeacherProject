df47e261a399b12f9136bf3af789f2016c9e506c
dbeecroft <teddie79@gmail.com>
2016-07-26 13:08:51 -0600

    Changed Sandwich name

--- TeacherProject/main.cpp
+++ TeacherProject/main.cpp
@@ -9,6 +9,7 @@
 // Preprocessor - happens before compiling (ignores #)
 #include <iostream>
 #include <string>
+#include "Sandwich.h"
 
 // using a specific library
 using namespace std;
@@ -16,33 +17,48 @@ using namespace std;
 // starts and runs project
 int main()
 {
-    // insert code here...
+    Sandwich mySandwich;
+    cout << mySandwich.getName() << endl;
+    cout << "Let's rename my sandwich." << endl;
+    cout << "Type in a new name." << endl;
+    string tempName;
+    //  cin >> tempName;
+    getline(cin, tempName);
+    mySandwich.setName (tempName);
+    cout << "The new name is: " << mySandwich.getName() << endl;
     
-    // will display on the console
-    cout << "Hello, World!\n";
-    
-    double myFraction;
-    
-    cout << myFraction << endl;
-    myFraction = 3.15;
-    /* = is a shover. Take what's on the 
-     right and 'shove' it into the left. Assignment.*/
-    
-    cout << myFraction << endl;
-    
-    int myFavoriteNumber;
-    cout << myFavoriteNumber << endl;
-    cin >> myFavoriteNumber;
-    cout << myFavoriteNumber << endl;
-    
-    bool myBoolean = true;
-    cout << myBoolean << endl;
-    
-    string myWords;
-    myWords = "Howdy";
-    cout << myWords << endl;
-    
-    // program exited properly
     return 0;
     
 }// denotes scope
+
+
+
+
+
+// insert code here...
+
+// will display on the console
+//    cout << "Hello, World!\n";
+//
+//    double myFraction;
+//
+//    cout << myFraction << endl;
+//    myFraction = 3.15;
+//    /* = is a shover. Take what's on the
+//     right and 'shove' it into the left. Assignment.*/
+//
+//    cout << myFraction << endl;
+//
+//    int myFavoriteNumber;
+//    cout << myFavoriteNumber << endl;
+//    cin >> myFavoriteNumber;
+//    cout << myFavoriteNumber << endl;
+//
+//    bool myBoolean = true;
+//    cout << myBoolean << endl;
+//
+//    string myWords;
+//    myWords = "Howdy";
+//    cout << myWords << endl;
+
+// program exited properly
