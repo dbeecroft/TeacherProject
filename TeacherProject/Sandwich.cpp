6acc66aa39173f3469d97aecd768f8929b2c0658
dbeecroft <teddie79@gmail.com>
2016-07-26 12:25:08 -0600

    Add Sandwich file

---
+++ TeacherProject/Sandwich.cpp
@@ -0,0 +1,9 @@
+//
+//  Sandwich.cpp
+//  TeacherProject
+//
+//  Created by Denise Beecroft on 7/26/16.
+//  Copyright © 2016 Denise Beecroft. All rights reserved.
+//
+
+#include "Sandwich.h"
--- TeacherProject/main.cpp
+++ TeacherProject/main.cpp
@@ -1,15 +1,48 @@
-//
-//  main.cpp
-//  TeacherProject
-//
-//  Created by Henrichsen, Cody on 7/26/16.
-//  Copyright © 2016 Denise Beecroft. All rights reserved.
-//
+/*
+  main.cpp
+  TeacherProject
 
+  Created by Denise Beecroft on 7/26/16.
+  Copyright © 2016 Denise Beecroft. All rights reserved.
+*/
+
+// Preprocessor - happens before compiling (ignores #)
 #include <iostream>
+#include <string>
 
-int main(int argc, const char * argv[]) {
+// using a specific library
+using namespace std;
+
+// starts and runs project
+int main()
+{
     // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    // will display on the console
+    cout << "Hello, World!\n";
+    
+    double myFraction;
+    
+    cout << myFraction << endl;
+    myFraction = 3.15;
+    /* = is a shover. Take what's on the 
+     right and 'shove' it into the left. Assignment.*/
+    
+    cout << myFraction << endl;
+    
+    int myFavoriteNumber;
+    cout << myFavoriteNumber << endl;
+    cin >> myFavoriteNumber;
+    cout << myFavoriteNumber << endl;
+    
+    bool myBoolean = true;
+    cout << myBoolean << endl;
+    
+    string myWords;
+    myWords = "Howdy";
+    cout << myWords << endl;
+    
+    // program exited properly
     return 0;
-}
+    
+}// denotes scope
